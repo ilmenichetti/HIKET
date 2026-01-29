d86716cf30bcdd3b5b17052e812b384831e5ab65
ilmenichetti <ilmenichetti@gmail.com>
2026-01-29 15:34:32 +0200

    Bit of documentation updates

--- main.R
+++ main.R
@@ -4,9 +4,10 @@
 #load multi-model wrapper functions
 source("./Models_functions/multi_model.R")
 
-#TODO: Q has now a transient temperature sensitivity but linear, maybe it can be updated
-#TODO: Yasso 07 and 15  are initialized via spinup
-
+# TODO: models still give a bit too different results, find ways for testing
+# TODO: Q has now a transient temperature sensitivity but linear, maybe it can be updated
+# TODO: Yasso 07 and 15  are initialized via spinup
+# TODO documentation is still inconsistent, needs more worl
 
 #For testing, run
 source("./Models_functions/synthetic_data_template.R")
