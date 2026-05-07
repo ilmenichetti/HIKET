22e32197437dcd52dc58a1d5786e44487b92460f
Lorenzo Menichetti <menichet@puhti-login11.bullx>
2026-05-07 14:32:33 +0300

    Add SLURM submission scripts

---
+++ Calibration_real_data/hiket_pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=hiket_pipe
+#SBATCH --account=project_2019134
+#SBATCH --partition=small
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2000
+#SBATCH --time=01:00:00
+#SBATCH --output=Calibration_real_data/progress_logs/pipeline_%j.out
+#SBATCH --error=Calibration_real_data/progress_logs/pipeline_%j.err
+
+cd /scratch/project_2019134/HIKET/
+srun apptainer_wrapper exec Rscript --no-save Calibration_real_data/run_hiket_pipeline.R --skip-calibration
