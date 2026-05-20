1c33fc62e2ecb6f31c688deb8b4d99c060e8c9fd
ilmenichetti <ilmenichetti@gmail.com>
2026-05-20 15:05:05 +0300

    longer runs for production calib

--- Calibration_real_data_transient/calib_config.R
+++ Calibration_real_data_transient/calib_config.R
@@ -3,6 +3,6 @@
 
 N_PLOTS_TEST <- NA    # NA = full dataset; set to e.g. 20L for quick tests
 N_CHAINS     <- 5L
-N_ITER       <- 20000L
-N_BURNIN     <- 2000L
+N_ITER       <- 50000L
+N_BURNIN     <- 5000L
 N_LOG        <- 200L
